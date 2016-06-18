24aa5507686fd474b257c817953ce2543d73e771
fangxian <fangxian1108@gmail.com>
2016-06-18 20:19:10 +0200

    to do test

--- Image/main.cpp
+++ Image/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 #include <cstdint>
 #include <stdlib.h>
 #include "Dim2u.h"
@@ -6,35 +7,17 @@
 
 using namespace std;
 using namespace Im;
-//generate random image
+//generate random image -- done, has moved to the image class
 //test image class
-//gaussian filter
+//to do unit test
 
 
 
 
-void generateImage(Dim2u dim2u, uint32_t* data)
-{
-    uint32_t* buffer;
-    const uint32_t length = dim2u.width * dim2u.height;
-    uint32_t i;
-    buffer = (uint32_t*)data;
-    for(i = 0; i < length; i++)
-    {
-        buffer[i] = (rand())%(255-0) + 0;
-    }
-}
 
 
 int main()
 {
-    Dim2u dim2u(80,80);
-    uint32_t data[6400];
-    uint8_t* src = new uint8_t[6400];
-    generateImage(dim2u, data);
-    printf("%d", (uint8_t)data[2]);
-    memcpy(src, (uint8_t *)data, 6400);
-    cout << "Hello, World!" << endl;
-    delete[] src;
+    cout << "hello world!" << endl;
     return 0;
 }
